fd26dbe86e5e4125513008597229cd08fc65333d
Ashwin Bhola <ab8084@nyu.edu>
2021-09-26 21:06:21 +0530

    changing model architecture

--- policies.py
+++ policies.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -7,11 +8,11 @@ class MLPPolicy(nn.Module):
     		observation_space_dim: int,
     		action_space_dim: int):
         super().__init__()
-        self.l1 = nn.Linear(observation_space_dim, 128)
-        self.dropout = nn.Dropout(p=0.6)
-        self.l2 = nn.Linear(128, action_space_dim)
+        self.l1 = nn.Linear(observation_space_dim, 8)
+        self.dropout = nn.Dropout(p=0.2)
+        self.l2 = nn.Linear(8, action_space_dim)
 
     def forward(self, batch):
-        hidden = F.relu(self.dropout(self.l1(batch)))
+        hidden = torch.tanh(self.dropout(self.l1(batch)))
         out = self.l2(hidden)
         return F.softmax(out, dim=-1)
