41c14ab0ea3f484ae65a2b3a395ed2f19fc62038
Ashwin Bhola <ab8084@nyu.edu>
2021-09-28 09:09:19 +0530

    changed SGD to adam and incorporated sutton importance weighted

--- agents.py
+++ agents.py
@@ -22,7 +22,7 @@ class DavidAgent():
             observation_space_dim=self.observation_space_dim,
             action_space_dim=self.action_space_dim
         )
-        self.optimizer = optim.SGD(self.policy.parameters(), lr=1e-03)
+        self.optimizer = optim.Adam(self.policy.parameters())
         self.max_steps = 100000
     
     def select_action(
@@ -64,6 +64,8 @@ class DavidAgent():
             self,
             max_episodes: int,
             save_pth: str,
+            logging: bool=True,
+            save: bool=True,
             checkpoint: int=100) -> List[float]:
         '''
         Plays `max_episodes` number of episodes
@@ -99,7 +101,8 @@ class DavidAgent():
             training_returns.append(returns[0])
             if returns[0] >= best_return:
                 best_return = returns[0]
-                torch.save(self.policy, save_pth)
+                if save:
+                    torch.save(self.policy, save_pth)
                            
             running_reward = running_reward * 0.9 +  returns[0] * 0.1
             
@@ -114,7 +117,7 @@ class DavidAgent():
             policy_loss.backward()
             self.optimizer.step()
             
-            if (ep_idx + 1) % checkpoint == 0:
+            if logging and (ep_idx + 1) % checkpoint == 0:
                 print(f"Episode: {ep_idx + 1} | score: {running_reward}")
         return training_returns
 
@@ -132,7 +135,7 @@ class SuttonAgent():
             observation_space_dim=self.observation_space_dim,
             action_space_dim=self.action_space_dim
         )
-        self.optimizer = optim.SGD(self.policy.parameters(), lr=5e-04)
+        self.optimizer = optim.Adam(self.policy.parameters())
         self.max_steps = 100000
     
     def select_action(
@@ -180,6 +183,8 @@ class SuttonAgent():
             self,
             max_episodes: int,
             save_pth: str,
+            save: bool=True,
+            logging: bool=True,
             checkpoint: int=100) -> List[float]:
         '''
         Plays `max_episodes` number of episodes
@@ -216,7 +221,8 @@ class SuttonAgent():
             training_returns.append(returns[0])
             if returns[0] >= best_return:
                 best_return = returns[0]
-                torch.save(self.policy, save_pth)
+                if save:
+                    torch.save(self.policy, save_pth)
                            
             running_reward = running_reward * 0.9 +  returns[0] * 0.1
             actions = episode['actions']
@@ -230,7 +236,129 @@ class SuttonAgent():
                 loss.backward()
                 self.optimizer.step()
             
-            if (ep_idx + 1) % checkpoint == 0:
+            if logging and (ep_idx + 1) % checkpoint == 0:
+                print(f"Episode: {ep_idx + 1} | score: {running_reward}")
+        return training_returns
+
+
+class SuttonAgentImportanceWeighted():
+    def __init__(self, env_name: str):
+        '''
+        Args
+            env_name: Gym environment to train the model on
+        '''
+        self.env = gym.make(env_name)
+        self.observation_space_dim = self.env.observation_space._shape[0]
+        self.action_space_dim = self.env.action_space.n
+        self.policy = MLPPolicy(
+            observation_space_dim=self.observation_space_dim,
+            action_space_dim=self.action_space_dim
+        )
+        self.optimizer = optim.Adam(self.policy.parameters())
+        self.max_steps = 100000
+    
+    def select_action(
+            self,
+            state: Iterator[float]) -> int:
+        '''
+        Given a `state`, samples an action using
+        `self.policy`. Returns the action sampled
+        '''
+        state_tensor = torch.tensor(state).unsqueeze(0)
+        action_probs = self.policy(state_tensor)
+        try:
+            m = Categorical(action_probs)
+        except:
+            print(action_probs)
+            m = Categorical(action_probs)
+        action = m.sample()
+        return action.item(), action_probs[0][action]
+    
+    def play_episode(self) -> Dict[str, List[float]]:
+        '''
+        Runs one episode of the environment
+        using `self.policy`. Returns states, actions,
+        and rewards observed during this episode
+        '''
+        self.policy.eval()
+        with torch.no_grad():
+            state = self.env.reset()
+            episode = {
+                'states': [],
+                'actions': [],
+                'probs' : [],
+                'rewards': []
+            }
+            for t in range(0, self.max_steps):
+                episode['states'].append(state)
+                action, prob = self.select_action(state)
+                episode['actions'].append(action)
+                episode['probs'].append(prob)
+                state, reward, done, info = self.env.step(action)
+                episode['rewards'].append(reward)
+                if done:
+                    break
+        return episode
+    
+    def update_policy(
+            self,
+            max_episodes: int,
+            save_pth: str,
+            save: bool=True,
+            logging: bool=True,
+            checkpoint: int=100) -> List[float]:
+        '''
+        Plays `max_episodes` number of episodes
+        and after each episode, updates the 
+        `self.policy` using the rewards generated for
+        that episode and the actions taken. The policy update
+        happens only after one complete episode has been
+        played using the current `self.policy` and the number of
+        updates done for any episode is equal to the length of the
+        episode (each state, action, reward triplet corresponds to one
+        update)
+        Args:
+            max_episodes: Total number of episodes to train
+            save_pth: Save the best agent trained to this path
+            checkpoint: number of episodes after which the running
+                rewards are logged
+        Returns:
+        Return of each episode played
+        '''
+        training_returns = []
+        running_reward = 0
+        best_return = 0
+        for ep_idx in range(max_episodes):
+            
+            episode = self.play_episode()
+            self.policy.train()
+            
+            returns = []
+            future_return = 0
+            for r in episode['rewards'][::-1]:
+                future_return += r
+                returns.insert(0, future_return)
+
+            training_returns.append(returns[0])
+            if returns[0] >= best_return:
+                best_return = returns[0]
+                if save:
+                    torch.save(self.policy, save_pth)
+                           
+            running_reward = running_reward * 0.9 +  returns[0] * 0.1
+            actions = episode['actions']
+            
+            for i in range(len(actions)):
+                self.optimizer.zero_grad()
+                state = torch.tensor(episode['states'][i]).unsqueeze(0)
+                probs = self.policy(state)[0]
+                imp_weight = probs[actions[i]].detach().item() / episode['probs'][i]
+                log_prob = torch.log(probs[actions[i]])
+                loss = - returns[i] * log_prob * imp_weight
+                loss.backward()
+                self.optimizer.step()
+            
+            if logging and (ep_idx + 1) % checkpoint == 0:
                 print(f"Episode: {ep_idx + 1} | score: {running_reward}")
         return training_returns
             
